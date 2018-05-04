ac7845ef9d680b2bd00c9a29a5758ee57f958b1a
Don Nguyen <thedonwind69@gmail.com>
2018-05-03 19:55:32 -0700

    adding

--- js/game.js
+++ js/game.js
@@ -1,6 +1,9 @@
 
 class Game {
 
+    constructor () {
+        this.yourHand = [];
+    }
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 console.log("black jack bitch");
-console.log('sex');
-console.log("black jack bitch");
-console.log('sex');
+
 var Game = require('./game.js');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rootEl = document.getElementById('rootEl');
+    console.log(rootEl);
+
+})
+
